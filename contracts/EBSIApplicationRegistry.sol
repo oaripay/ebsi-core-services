7556fddc122ec37155a21252d9bd64adff06372d
Adrian Bancu <abancu@yahoo.com>
2019-12-13 13:06:29 +0200

    added smart contracts

---
+++ contracts/EBSIApplicationRegistry.sol
@@ -0,0 +1,92 @@
+pragma solidity ^0.5.9;
+
+import "./roles/roles/Ownable.sol";
+
+contract EBSIApplicationRegistry is Ownable {
+
+    struct ApplicationRegistry {
+        string applicationPublicKey;
+
+        string applicationName;
+        // keccak - sha3 - of applicationName keccak256(abi.encodePacked(applicationName)
+        bytes32 appCode;
+        mapping (bytes32 => AuthApp) authList;
+    }
+
+    struct AuthApp {
+        string authName;
+        bool status;
+
+    }
+
+    mapping (bytes32 => ApplicationRegistry) registry;
+    mapping (bytes32 => bytes32[]) mappingAuthList;
+
+
+
+    function addApplication (string calldata pubKey, string calldata name)
+    external
+    onlyOwner
+    returns (bool)
+    {
+        ApplicationRegistry memory appRegistry;
+
+        appRegistry.applicationPublicKey = pubKey;
+        appRegistry.applicationName = name;
+        appRegistry.appCode = keccak256(abi.encodePacked(name));
+        registry[keccak256(abi.encodePacked(name))] = appRegistry;
+        return true;
+    }
+
+    function addNewAuthorization (string calldata applicationName, string calldata authorizationName, bool status)
+    external
+    onlyOwner
+    returns (bool)
+    {
+        bytes32 appCode = keccak256(abi.encodePacked(applicationName));
+        bytes32 authKey = keccak256(abi.encodePacked(authorizationName));
+
+        require (registry[appCode].appCode != '', 'This does not exist');
+        require (registry[authKey].appCode != '', 'Application you want to add as access does not exist');
+
+        AuthApp memory authorizedApp;
+        authorizedApp.status = status;
+        authorizedApp.authName = authorizationName;
+        registry[appCode].authList[authKey] = authorizedApp;
+        mappingAuthList[appCode].push(authKey);
+
+        return true;
+    }
+
+
+    function getApplicationPublicKey (string calldata applicationName)
+    external
+    view
+    returns (string memory)
+    {
+        bytes32 appName = keccak256(abi.encodePacked(applicationName));
+        require (registry[appName].appCode != '', 'Application does not exist');
+        return registry[appName].applicationPublicKey;
+    }
+
+    function getAuthorizedApps (string calldata applicationName)
+    external
+    view
+    {
+        bytes32 appName = keccak256(abi.encodePacked(applicationName));
+        require (registry[appName].appCode != '', 'Application does not exist');
+
+
+
+        for (uint256 i = 0; i < mappingAuthList[appName].length; i++)
+        {
+
+        }
+
+    }
+
+
+
+
+
+}
