7f39b0780a8a6b36d14b46b77cecd89364adf396
Rohy Raniriharison <rohy.raniriharison@ts.fujitsu.com>
2020-01-22 14:16:09 +0100

    test local storage

--- public/javascripts/check.js
+++ public/javascripts/check.js
@@ -1,5 +1,39 @@
 console.log('*****************');
+function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
 
+if (storageAvailable('localStorage')) {
+  // Yippee! We can use localStorage awesomeness
+  console.log('// Yippee! We can use localStorage awesomeness');
+  check();
+}
+else {
+  // Too bad, no localStorage for us
+  console.log('// Too bad, no localStorage for us');
+  alert('your privacy protocol doesn\'t allow some functionality!!!');
+}
 
 function check() {
   var pathname = window.location.pathname;
@@ -24,4 +58,3 @@ function check() {
   }
 }
 
-check();
