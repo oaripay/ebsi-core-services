48c5ecb61a33d74f508343f9bcc0e87fa28a8d4e
Julian GONZALEZ AGUDELO <Julian.GONZALEZ-AGUDELO@ext.ec.europa.eu>
2020-02-25 08:21:31 +0000

    ebsiint-384: truffle private key as env variable

--- smart-contracts/truffle-config.js
+++ smart-contracts/truffle-config.js
@@ -1,13 +1,13 @@
 const PrivateKeyProvider = require("truffle-hdwallet-provider");
-const privateKey = "8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63";
-const privateKeyProvider = new PrivateKeyProvider(privateKey, "https://www.intebsi.xyz/jsonrpc");
+if (!process.env.PRIVATE_KEY) throw new Error("PRIVATE_KEY is not defined");
+const privateKey = process.env.PRIVATE_KEY;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
     ebsi: {
-      provider: () => new PrivateKeyProvider(privateKey, "https://ebsi.xyz/jsonrpc"),
+      provider: () => new PrivateKeyProvider(privateKey, "https://www.ebsi.xyz/jsonrpc"),
       network_id: "*",
       gas:"0x1ffffffffffffe",
       gasPrice: 0
