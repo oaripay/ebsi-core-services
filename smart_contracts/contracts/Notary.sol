5c2b1a9f8b788882b1ad12bf4b0c5dd2c8ed937f
Julian GONZALEZ AGUDELO <Julian.GONZALEZ-AGUDELO@ext.ec.europa.eu>
2020-02-27 15:24:10 +0000

    EBSIINT-384: lastBlockREC in events to facilitate searches

--- smart_contracts/contracts/Notary.sol
+++ smart_contracts/contracts/Notary.sol
@@ -2,17 +2,19 @@ pragma solidity ^0.5.8;
 
 contract Notary {
   mapping(bytes32 => address) public record;
+  uint public lastBlockREC = 0;
   uint public totalRecords;
 
   // Event to record
-  event REC(bytes32 indexed docHash, address indexed signer, uint indexed timestamp);
+  event REC(bytes32 indexed docHash, address indexed signer, uint indexed lastBlock);
 
   // add a single record
   function addRecord(bytes32 docHash) public {
     require(record[docHash] == address(0), "This hash already exists");
 
     record[docHash] = msg.sender;
-    emit REC(docHash, msg.sender, now);
+    emit REC(docHash, msg.sender, lastBlockREC);
+    lastBlockREC = block.number;
 
     totalRecords ++;
   }
