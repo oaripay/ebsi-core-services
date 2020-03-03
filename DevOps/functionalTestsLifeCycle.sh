a82129dc6057472abf9c5299e4877e1ed17ce040
Julian GONZALEZ AGUDELO <Julian.GONZALEZ-AGUDELO@ext.ec.europa.eu>
2020-03-03 17:07:03 +0000

    merge copy fix/EBSIINT-384

---
+++ DevOps/functionalTestsLifeCycle.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+# TODO:(0) Move this script to migrations project and reuse.
+function throw {
+   echo "$(basename $0)"
+   echo "    Aborting due to error:"
+   echo "    $1"
+   exit 1
+}
+
+cd $(dirname $0)/..
+if [[  ! -d test ]] ; then
+  throw "/test dir not found. CWD: $PWD"
+fi
+
+# Use project name AS RUN_FILE prefix
+RUN_FILE="$(echo $(pwd) | sed "s/^.*\///")_ganache_var_run"
+RUN_FILE=$(echo $RUN_FILE | sed "s/ /_/")
+
+LOG_FILE="ganache_functionalTests.log"
+
+function funCheckTruffleOrThrow { # TODO:(0)
+ which truffle || throw "Truffle not found. Use 'sudo npm install -g truffle@5.1.12' to install it"
+}
+
+GANACHE_PORT=18545  # Must match truffle-config.js 'functionalTestNet' settings
+
+function funTestNodeModulesOrInstall {
+  TMP_FILE=$(mktemp --dry-run)
+  echo "npm install STDOUT/STDERR reditected to '$TMP_FILE'"
+  npm install 1>$TMP_FILE 2>&1
+  npm audit # TODO:(0) Abort on critical audit-errors?
+}
+
+function funStartGanacheFunctionalTest {
+  # TODO:(?) Forze serializations of allow parallel executions. By default parallel is
+  #     allowed but not sure if ganache-cli allow concurrent client
+  ps -ef | egrep "${RUN_FILE}.*ganache.cl[i]"
+  if [[ $? == 0 ]] ; then
+      echo "$RUN_FILE found and associated process still running."
+  else
+      echo "Launching new ganache instance"
+    rm -f $RUN_FILE
+    node ./node_modules/ganache-cli/cli.js \
+        --port ${GANACHE_PORT} 1>${LOG_FILE} 2>&1 &
+    if [[ $? != 0 ]] ; then
+      throw "Couldn't launch Ganache for testing. Check ${LOG_FILE} for more info"
+    fi
+    echo -n $! >> ${RUN_FILE}
+  fi
+}
+
+function funStopGanacheFunctionalTest {
+  kill $(cat ${RUN_FILE})
+  rm -f ${RUN_FILE}
+}
+
+function funLaunchTestsFunctionalTest {
+  export PRIVATE_KEY="DUMMY_NOT_USED"
+  truffle test --network functionalTestNet
+}
+
+funCheckTruffleOrThrow
+funTestNodeModulesOrInstall
+funStartGanacheFunctionalTest
+funLaunchTestsFunctionalTest
+funStopGanacheFunctionalTest
---
+++ test/FullJourney.js
@@ -0,0 +1,91 @@
+/* 
+ * REF 1: https://github.com/trufflesuite/truffle/issues/314
+ * REF 2: https://github.com/trufflesuite/truffle-contract/issues/117
+ * REF 3: https://github.com/trufflesuite/truffle/blob/next/packages/truffle-contract/test/events.js#L118-L143 
+ * REF 4: https://ethereum.stackexchange.com/questions/54967/how-to-get-only-past-2-days-events-using-getpastevents-everytime?rq=1
+ *        { fromBlock: (await web3.eth.getBlockNumber()) - 12343,  toBlock: "latest" }); 
+ *                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *                     latest block
+ * https://web3js.readthedocs.io/en/1.0/web3-eth-contract.html#getpastevents
+ */
+
+// https://ethereum-magicians.org/t/removing-or-increasing-the-contract-size-limit/3045
+
+const BN_ZERO = new web3.utils.BN(0)
+
+
+var assertCount = 0; // Test Stats purposes only
+const assert_equal   = function(a,b,c) { assertCount++ ; assert.equal  (a,b, c); }
+// assert_err  = function(a,b,c) { assertCount++ ; assert.isTrue  (a.reason.indexOf(b)==0, c); }
+const assert_err  = function(a,b,c) { assertCount++ ; assert.isTrue  (a!==undefined && a!=null);  }
+
+const assert_isTrue  = function(a,b  ) { assertCount++ ; assert.isTrue (a,b   ); }
+const assert_isFalse = function(a,b  ) { assertCount++ ; assert.isFalse(a,b   ); }
+
+const MUST_NOT_EXECUTE = { reason : "This code must never be executed. An exception must be raised" }
+
+const NotaryContract = artifacts.require("Notary");
+const ethers = require("ethers");
+
+JSON.stringifySec = function (circ) {
+    cache = []
+    result = JSON.stringify(circ, function(key, value) {
+        if (typeof value === 'object' && value !== null) {
+            if (cache.indexOf(value) !== -1) {
+                // Duplicate reference found, discard key
+                return
+            }   
+            // Store value in our collection
+            cache.push(value)
+        }   
+        return value
+    })  
+    cache = null
+    return result                                                                                           
+}
+
+
+let web3Provider;
+contract('RealEstateControllerFullJourney', accounts => {
+  const ACCT0 = accounts[0]; const FROM_ACCT0 = { from : ACCT0 }
+  const ACCT1 = accounts[1]; const FROM_ACCT1 = { from : ACCT1 }   
+  const ACCT2 = accounts[2]; const FROM_ACCT2 = { from : ACCT2 }       
+
+  let NOTARY ; // proxy class to published smart-contract instance
+  before('setup Contract', async() => {
+    NOTARY  = await NotaryContract.new()   
+  })
+
+  after('PRINT TEST STATS', () => {
+    console.log("assertCount:"+assertCount)
+  })
+
+  describe('SMART-CONTRACT functionality', () => {
+    it("SHOULD notarize document properly", async () => {
+        // REF: https://docs.ethers.io/ethers.js/html/api-utils.html
+        const hash01 = ethers.utils.hashMessage("INPUT_STRING")
+        const result = await NOTARY.addRecord(hash01)
+        assert_equal(result.receipt.logs.length,1, "")
+        const log0   = result.receipt.logs[0]
+        assert_equal(log0.event    , "REC", "") 
+        assert_equal(log0.args["0"], hash01 )
+        assert_equal(log0.args["1"], ACCT0 ,"")
+
+        // TODO:(?)
+        //    Use etherjs library if possible to allow it to be compliant with current backend code.
+      }
+    )
+   })
+
+//  it("SHOULD failt to add document array", async () => {
+//    // addMultipleRecords(uint[] memory zz)
+//  })
+ 
+// TODO:(0) disabled. (EBSIv2)
+//  it('SHOULD allow to transfer Ownership', async () => {
+//     await NOTARY.transferOwnership(ACCT1); 
+//  })
+
+//})
+})
+
