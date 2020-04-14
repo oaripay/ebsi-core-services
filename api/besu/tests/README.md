e160552d4641f95864ead9fac58871bec77dd651
Julian GONZALEZ AGUDELO <Julian.GONZALEZ-AGUDELO@ext.ec.europa.eu>
2020-04-14 17:05:02 +0200

    EBSIINT-1184: Besu Tests

--- api/besu/tests/README.md
+++ api/besu/tests/README.md
@@ -1,49 +1,11 @@
-# Tests EBSI API
+# Tests Hyperledger Besu
 
-This folder contains a set of files to test the different APIs.
+This folder contains a set of tests for Besu API.
 
-## Launch the api
+Set the environmental variable `EBSI_ENV` to integration, development or production to determine the location of Besu RPC node.
 
-If you want to test it locally launch a new api.
-
-Before launching the EBSI API configure the APIs you want to enable in the environmental variables (all enabled by default), and the port (8080 by default):
+Then run
 
 ```
-APIS = ON                            # all enabled
-API_BESU = ON                        # besu
-API_NOTARY = ON                      # notary
-API_FILE_STORAGE = ON                # file storage
-API_WALLET_REQUEST_STORAGE = ON      # wallet request storage
-API_WALLET_HISTORICAL_STORAGE = ON   # wallet historical storage
-API_KEY_VALUE_STORAGE = ON           # key value
-PORT = 8080
-```
-
-Start the EBSI API. Example to start only Besu api:
-
-```
-~$ APIS=OFF API_BESU=ON node index.js
-```
-
-## Define private key
-
-Define the private key and app name in the environmental variables. This app needs to be in the trusted list of applications:
-
-```
-TEST_APP_NAME="ebsi-wallet"
-TEST_APP_PRIVATE_KEY=3afbada2b09..."
-```
-
-## Test the api
-
-Run
-
-```
-EBSI_ENV=development npm run test
-```
-
-Depending on the environment EBSI_ENV (production, development, integration, local) the test will try to connect with the corresponding API. If you want to connect to a specific api use env TEST_API
-
-```
-TEST_API="https://api.ebsi.xyz"
+npm run test
 ```
--- api/besu/tests/besu.test.js
+++ api/besu/tests/besu.test.js
@@ -1,167 +1,25 @@
-var ethers = require("ethers");
-var jose = require("jose");
+const ethers = require("ethers");
+const jose = require("jose");
 require("dotenv").config();
 
-var { sleep, axios2 } = require("./utils");
-var config = require("./config");
+const config = require("../config");
+const {
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+} = require("../errors");
+const utils = require("../utils");
+const auth = require("../auth");
+const controller = require("../controller");
 
-var wallet, provider;
-var token, badToken, selfToken;
-var randomHash, txId;
+const provider = new ethers.providers.JsonRpcProvider(config.besuRPCNode);
+const wallet = ethers.Wallet.createRandom();
+const r = Math.random().toString(36);
+const randomHash = ethers.utils.keccak256(Buffer.from(r, "utf8"));
 
-/*
- * Initialization
- */
+const ANONYMOUS = false;
 
-beforeAll(() => {
-  // wallet and provider
-  provider = new ethers.providers.JsonRpcProvider(config.api.besu);
-  wallet = ethers.Wallet.createRandom();
-
-  // Self token for login
-  var payload = {
-    iss: config.app_name,
-    aud: "ebsi-besu"
-  };
-  var key = config.private_key;
-  selfToken = jose.JWT.sign(payload, key, { expiresIn: "15 minutes" });
-  badToken = selfToken;
-
-  // Random hash
-  var r = Math.random().toString(36);
-  randomHash = ethers.utils.keccak256(Buffer.from(r, "utf8"));
-});
-
-/*
- * Tests
- */
-
-test("Ping to Besu API", async () => {
-  var response = await callBesuAPI();
-  expect(response).toBeDefined();
-});
-
-test("Ping to Notary API - get contract info", async () => {
-  var response = await axios2.get(config.api.notary);
-  expect(response.status).toBe(200);
-  expect(response.data).toEqual(
-    expect.objectContaining({
-      notary: expect.objectContaining({
-        address: expect.any(String),
-        abi: expect.arrayContaining([])
-      })
-    })
-  );
-});
-
-test("getBalance", async () => {
-  var response = await callBesuAPI("eth_getBalance", [
-    wallet.address,
-    "latest"
-  ]);
-  expect(response.status).toEqual(200);
-  expect(response.data).toEqual(respBesu("0x0"));
-});
-
-test("getBlockByNumber", async () => {
-  var response = await callBesuAPI("eth_getBlockByNumber", ["11", true]);
-  expect(response.status).toBe(200);
-  expect(response.data).toEqual(
-    respBesu(
-      expect.objectContaining({
-        number: expect.any(String),
-        hash: expect.any(String),
-        transactions: expect.arrayContaining([])
-      })
-    )
-  );
-});
-
-test("Get net_version", async () => {
-  var response = await callBesuAPI("net_version", []);
-  expect(response.status).toBe(200);
-  expect(response.data).toEqual(respBesu(expect.any(String)));
-});
-
-test("incorrect_method is rejected", async () => {
-  var response = await callBesuAPI("incorrect_method", []);
-  expect(response.status).toBe(400);
-});
-
-test("sendRawTransaction without authentication not allowed", async () => {
-  var response = await callBesuAPI("eth_sendRawTransaction", ["0x000"]);
-  expect(response.status).toBe(403);
-});
-
-test("sendRawTransaction with bad auth is rejected", async () => {
-  var response = await callBesuAPI(
-    "eth_sendRawTransaction",
-    ["0x000"],
-    badToken
-  );
-  expect(response.status).toBe(403);
-});
-
-test("Login in Besu API", async () => {
-  var response = await _login(selfToken);
-  expect(response.status).toBe(200);
-  expect(response.data).toEqual(
-    expect.objectContaining({
-      token: expect.any(String)
-    })
-  );
-  token = response.data.token;
-});
-
-test("Read an empty hash using the Notary API", async () => {
-  var response = await axios2.get(config.api.notary + "/" + randomHash);
-  expect(response.status).toBe(404);
-});
-
-test("Notarize a hash (sendRawTransaction + auth)", async () => {
-  var sgnTx = await getNotarizeTransaction(randomHash);
-  var response = await callBesuAPI("eth_sendRawTransaction", [sgnTx], token);
-  expect(response.status).toBe(200);
-  expect(response.data).toEqual(respBesu(expect.any(String)));
-  txId = response.data.result;
-});
-
-test("Check good receipt after 2 seconds", async () => {
-  await sleep(2000);
-  var response = await callBesuAPI("eth_getTransactionReceipt", [txId]);
-  expect(response.status).toBe(200);
-  expect(response.data).toEqual(
-    respBesu(
-      expect.objectContaining({
-        blockHash: expect.any(String),
-        blockNumber: expect.any(String),
-        from: expect.any(String)
-      })
-    )
-  );
-  var from = response.data.result.from.toLowerCase();
-  expect(from).toBe(wallet.address.toLowerCase());
-});
-
-test("Read the notarized hash using the Notary API", async () => {
-  var response = await axios2.get(config.api.notary + "/" + randomHash);
-  expect(response.status).toBe(200);
-  expect(response.data).toEqual({
-    hash: randomHash,
-    timestamp: expect.any(Number),
-    registeredBy: expect.any(String),
-    blockNumber: expect.any(Number)
-  });
-  expect(response.data.timestamp).not.toBe("0");
-  var registeredBy = response.data.registeredBy.toLowerCase();
-  expect(registeredBy).toBe(wallet.address.toLowerCase());
-});
-
-test("Reject the deployment of a new smart contract", async () => {
-  var sgnTx = await getDeployTransaction();
-  var response = await callBesuAPI("eth_sendRawTransaction", [sgnTx], token);
-  expect(response.status).toBe(403);
-});
+let txId;
 
 /*
  * Functions
@@ -171,52 +29,148 @@ function respBesu(result) {
   return expect.objectContaining({
     jsonrpc: "2.0",
     id: expect.any(Number),
-    result: result
+    result,
   });
 }
 
-function _login(token) {
-  return axios2.get(config.api.besu + "/login", token);
-}
-
-function callBesuAPI(method, params, token) {
-  var data = {
+function callAPI(method, params, authenticated = true) {
+  const data = {
     jsonrpc: "2.0",
     method,
     params,
-    id: 1
+    id: 1,
   };
-  return axios2.post(config.api.besu, data, token);
+  return controller.besuRPC(data, authenticated);
 }
 
 async function getNotarizeTransaction(hash) {
-  var response = await axios2.get(config.api.notary);
-  var notary = response.data.notary;
-  var iface = new ethers.utils.Interface(notary.abi);
-  var transaction = {
+  const iface = new ethers.utils.Interface(config.notary.abi);
+  const transaction = {
     nonce: await provider.getTransactionCount(wallet.address),
     gasLimit: 221000,
     gasPrice: 0,
-    to: notary.address,
+    to: config.notary.address,
     value: 0,
-    data: iface.functions.addRecord.encode([hash])
-
-    // This ensures the transaction cannot be replayed on different networks
-    // chainId: provider.chainId
+    data: iface.functions.addRecord.encode([hash]),
   };
 
   return wallet.sign(transaction);
 }
 
 async function getDeployTransaction() {
-  var transaction = {
+  const transaction = {
     nonce: await provider.getTransactionCount(wallet.address),
     gasLimit: 221000,
     gasPrice: 0,
     to: "0x0000000000000000000000000000000000000000",
     value: 0,
-    data: "0x12345678901234567890"
+    data: "0x12345678901234567890",
   };
 
   return wallet.sign(transaction);
 }
+
+/*
+ * Tests
+ */
+
+describe("hyperledger Besu Test", () => {
+  it("getBalance", async () => {
+    expect.hasAssertions();
+    const params = [wallet.address, "latest"];
+    const result = await callAPI("eth_getBalance", params, ANONYMOUS);
+    expect(result).toStrictEqual(respBesu("0x0"));
+  });
+
+  it("getBlockByNumber", async () => {
+    expect.hasAssertions();
+    const params = ["11", true];
+    const result = await callAPI("eth_getBlockByNumber", params, ANONYMOUS);
+    expect(result).toStrictEqual(
+      respBesu(
+        expect.objectContaining({
+          number: expect.any(String),
+          hash: expect.any(String),
+          transactions: expect.arrayContaining([]),
+        })
+      )
+    );
+  });
+
+  it("get net_version", async () => {
+    expect.hasAssertions();
+    const result = await callAPI("net_version", [], ANONYMOUS);
+    expect(result).toStrictEqual(respBesu(expect.any(String)));
+  });
+
+  it("incorrect method is rejected", async () => {
+    expect.hasAssertions();
+    const t = async () => {
+      await callAPI("incorrect_method", [], ANONYMOUS);
+    };
+    expect(t).toThrow(BadRequestError);
+  });
+
+  it("sendRawTransaction without authentication not allowed", async () => {
+    expect.hasAssertions();
+    const t = async () => {
+      await callAPI("eth_sendRawTransaction", ["0x000"], ANONYMOUS);
+    };
+    expect(t).toThrow(UnauthorizedError);
+  });
+
+  it("session with Ledger API", async () => {
+    expect.hasAssertions();
+    const payload = {
+      iss: "test-app",
+      aud: config.API_NAME,
+    };
+    const privKey = utils.getJWKfromHex(wallet.privateKey);
+    const opts = { expiresIn: "15 minutes" };
+    const selfToken = jose.JWT.sign(payload, privKey, opts);
+
+    const result = auth.newSession(selfToken);
+    expect(result).toStrictEqual(
+      expect.objectContaining({
+        accessToken: expect.any(String),
+        tokenType: "Bearer",
+        expiresIn: 900, // 15 minutes
+        issuedAt: expect.any(String),
+      })
+    );
+  });
+
+  it("notarize a hash (sendRawTransaction + auth)", async () => {
+    expect.hasAssertions();
+    const sgnTx = await getNotarizeTransaction(randomHash);
+    const result = await callAPI("eth_sendRawTransaction", [sgnTx]);
+    expect(result).toStrictEqual(respBesu(expect.any(String)));
+    txId = result.result;
+  });
+
+  it("check good receipt after 2 seconds", async () => {
+    expect.hasAssertions();
+    await utils.sleep(2000);
+    const result = await callAPI("eth_getTransactionReceipt", [txId]);
+    expect(result).toStrictEqual(
+      respBesu(
+        expect.objectContaining({
+          blockHash: expect.any(String),
+          blockNumber: expect.any(String),
+          from: expect.any(String),
+        })
+      )
+    );
+    const from = result.result.from.toLowerCase();
+    expect(from).toBe(wallet.address.toLowerCase());
+  });
+
+  it("reject the deployment of a new smart contract", async () => {
+    expect.hasAssertions();
+    const sgnTx = await getDeployTransaction();
+    const t = async () => {
+      await callAPI("eth_sendRawTransaction", [sgnTx]);
+    };
+    expect(t).toThrow(ForbiddenError);
+  });
+});
