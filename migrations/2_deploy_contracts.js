aa1b130b29614c2b019a5e6c5978208e85bccbb1
Stefan <SAB>
2020-04-22 09:20:13 +0300

    added missing apps from migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -68,6 +68,14 @@ module.exports = async (deployer, network) => {
     await registryInstance.addApplication(app.pubKey, app.name)
 
     app = {
+        name: 'ebsi-eidas-bridge',
+        pubKey: 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUZZd0VBWUhLb1pJemowQ0FRWUZLNEVFQUFvRFFnQUVMeWlydDEvOWNuWm1Nd1Y2VjczSEhtaDhPSFdjZ05CVQpmL0U0T3M4Y1QyVWYrUjNsVzloQ2lQbTM3ZjlvakNxb2VyaG9HZm9NZ2lOSklSaEsrckRVZlE9PQotLS0tLUVORCBQVUJMSUMgS0VZLS0tLS0='
+    }
+    console.log('adding app ', app.name)
+
+    await registryInstance.addApplication(app.pubKey, app.name)
+
+    app = {
         name: 'ext-eca',
         pubKey: 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUZZd0VBWUhLb1pJemowQ0FRWUZLNEVFQUFvRFFnQUVHWXZGNDFuZm5KenlnWWFmZzlSMjB2TnhSejU1elFoaQpJb3hENEpwZG9JZm9yazFXN0wxTzhjKzNabVgrOU91ZXZETGJMQ1A1L2hnSTh2RHBjYUdzUEE9PQotLS0tLUVORCBQVUJMSUMgS0VZLS0tLS0='
     }
@@ -83,9 +91,38 @@ module.exports = async (deployer, network) => {
 
     await registryInstance.addApplication(app.pubKey, app.name)
 
+
+    app = {
+        name: 'ebsi-ledger',
+        pubKey: 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUZZd0VBWUhLb1pJemowQ0FRWUZLNEVFQUFvRFFnQUVqVFNEOWNPSWtMLzhnaks3L3gxbnpuVzB0SFM0TUpVMQpJMW1YR3lOYlF0Sm9FL3ozUkZ4YlU5SktHR28xYnA0a1A4M0VaaU5KYy9zS1dOQ3pra3BuRnc9PQotLS0tLUVORCBQVUJMSUMgS0VZLS0tLS0K'
+
+    }
+    console.log('adding app ', app.name)
+
+    await registryInstance.addApplication(app.pubKey, app.name)
+
+
+    app = {
+        name: 'trusted-apps-registry',
+        pubKey: 'LS0tLS1CRUdJTiBSU0EgUFVCTElDIEtFWS0tLS0tCk1JSUJDZ0tDQVFFQXhBV045QjlQM2l4RVVPZXM3d3g4UXJXOXpwcmp3TGRnMHRGRUkvUVFXd09adGo1dnNvUVUKYjVZbXF2TVRQT3kxSnpobUlzVU9CNFpmeUlhVkdvZ3B3bWxvUzlBd3VhMXd1alVBNW95K1BNcTEyV3doTnRMaApYQWdXTGRXaHdKSWpCMko5aHZ5b0pZWVIyZnp2TVdGeEZqV1BleXNHRmdJZTJiRW4zQVQ4b2IxNUwvL0I5MUpVCjRDNnFSUjh0SGhWbm1ZeXc0Y3V1RWJEVGs1Z0ltZ2dPbjRUWHdaOHJOT21SZkw4NlZmZTB1d2VuN0o4WjVocnEKQ2trVjVkMXdIeSt6RkxXdUZrcEo0N0ZQVWpESGExM09oeVFWOUlwYjlBWm9QVGUyZjhUOGdUVGlUNGQyZkQxQgp0bk9IT0ZmbGhyREM4YzAzRWwwdzZvaThSN2lFQU1TbHlRSURBUUFCCi0tLS0tRU5EIFJTQSBQVUJMSUMgS0VZLS0tLS0='
+
+    }
+    console.log('adding app ', app.name)
+
+    await registryInstance.addApplication(app.pubKey, app.name)
+
+    app = {
+        name: 'ebsi-idhub',
+        pubKey: 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUZZd0VBWUhLb1pJemowQ0FRWUZLNEVFQUFvRFFnQUVkT3NiWEZmRUpzZHdsc3JmZ0xaVHJrUG1iVU5uN2VUTgp5MGRqQW5UZTNoM2RGM09mZkVJTk1xUzlSRm5vMnVXS1RINzlsdTNMYmVnc3pGOHdWUFpnUGc9PQotLS0tLUVORCBQVUJMSUMgS0VZLS0tLS0='
+
+    }
+    console.log('adding app ', app.name)
+
+    await registryInstance.addApplication(app.pubKey, app.name)
+
     console.log ('adding access from all to all')
 
-    let apps = ['ebsi-besu', 'ebsi-fabric', 'ebsi-wallet', 'ebsi-notary', 'ebsi-storage', 'ebsi-diploma'];
+    let apps = ['ebsi-besu', 'ebsi-fabric', 'ebsi-wallet', 'ebsi-notary', 'ebsi-storage', 'ebsi-diploma', 'ebsi-ledger', 'trusted-apps-registry', 'ebsi-eidas-bridge', 'ebsi-idhub'];
 
     for(i=0; i<apps.length; i++) {
         for(j=0; j<apps.length; j++) {
