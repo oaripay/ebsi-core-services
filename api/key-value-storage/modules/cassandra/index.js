9a428f92562f8c7e0f55f5d59a4b335e81e54afc
Julian GONZALEZ AGUDELO <Julian.GONZALEZ-AGUDELO@ext.ec.europa.eu>
2020-04-22 16:17:23 +0200

    EBSIINT-710: key value storage using new RFC

---
+++ api/key-value-storage/modules/cassandra/index.js
@@ -0,0 +1,178 @@
+const cassandraDriver = require("cassandra-driver");
+const querystring = require("querystring");
+const jsonpatch = require("fast-json-patch");
+
+const config = require("../../../../config");
+const logger = require("../../../../logger");
+const {
+  BadRequestError,
+  NotFoundError,
+  KeyTooLargeError,
+  ValueTooLargeError,
+} = require("../../../../errors");
+
+const TABLE_KEY_VALUE_STORAGE = "key_value_storage";
+const MAX_SIZE_KEY = 256; // 256 bytes
+const MAX_SIZE_VALUE = 1024 * 1024; // 1 MB
+
+const cassandraConnection = config.cassandra.connection;
+const cassandra = new cassandraDriver.Client(cassandraConnection);
+
+function buildLink(store, before, after, pageSize) {
+  const query = {};
+  if (before) query["page[before]"] = before;
+
+  if (after) query["page[after]"] = after;
+
+  if (pageSize && pageSize !== config.DEFAULT_PAGE_SIZE)
+    query["page[size]"] = pageSize;
+
+  return `/storage/v1/stores/${store}/files?${querystring.stringify(query)}`;
+}
+
+async function getRecord(key) {
+  const query = `select value from ${TABLE_KEY_VALUE_STORAGE} where key = ? allow filtering`;
+  const result = await cassandra.execute(query, [key]);
+  return result.first();
+}
+
+async function setKey(key, value) {
+  let type;
+  let query;
+  let params;
+  let stringValue = value;
+  if (typeof value === "object") stringValue = JSON.stringify(value);
+
+  if (key.length > MAX_SIZE_KEY)
+    throw new KeyTooLargeError(
+      `Max size for 'key' is ${MAX_SIZE_KEY} bytes. Received ${key.length}`
+    );
+
+  if (stringValue.length > MAX_SIZE_VALUE)
+    throw new ValueTooLargeError(
+      `Max size for 'value' is ${MAX_SIZE_VALUE} bytes. Received ${stringValue.length}`
+    );
+
+  // check if the key exists
+  const record = await getRecord(key);
+
+  if (record) {
+    // update key
+    type = "update";
+    query = `update ${TABLE_KEY_VALUE_STORAGE} set value = ? where key= ? if exists`;
+    params = [stringValue];
+  } else {
+    // insert key
+    type = "insert";
+    query = `insert into ${TABLE_KEY_VALUE_STORAGE} (key, value) values (?, ?)`;
+    params = [key, stringValue];
+  }
+
+  const result = await cassandra.execute(query, params);
+
+  if (!result.info || !result.info.isSchemaInAgreement) {
+    logger.error(result);
+    throw new Error(
+      `Bad response from cassandra for '${type}' in key value storage`
+    );
+  }
+  const response = {};
+  response[key] = value;
+  return { type, result: response };
+}
+
+async function getKey(key) {
+  const record = await getRecord(key);
+  if (!record) throw new NotFoundError("key not found");
+  const stringValue = record.value;
+
+  // convert to JSON if it is the case
+  let value;
+  try {
+    value = JSON.parse(stringValue);
+  } catch (error) {
+    value = stringValue;
+  }
+  return value;
+}
+
+async function deleteKey(key) {
+  const record = await getRecord(key);
+  if (!record) throw new NotFoundError("key not found");
+  const query = `delete from ${TABLE_KEY_VALUE_STORAGE} where key = ? if exists`;
+
+  const result = await cassandra.execute(query, [key]);
+
+  if (!result.info || !result.info.isSchemaInAgreement) {
+    logger.error(result);
+    throw new Error(`Bad response from cassandra when deleting a key`);
+  }
+}
+
+async function patchKey(key, patch) {
+  const value = await getKey(key);
+  if (typeof value !== "object")
+    throw new BadRequestError(
+      `The key to be patched is not a JSON but a ${typeof value}`
+    );
+
+  let newValue;
+  try {
+    newValue = jsonpatch.applyPatch(value, patch).newDocument;
+  } catch (error) {
+    throw new BadRequestError(`Impossible to apply patch: ${error.message}`);
+  }
+
+  const { result } = await setKey(key, newValue);
+  return result;
+}
+
+async function getListKeys(q, store) {
+  let pageSize = config.DEFAULT_PAGE_SIZE;
+  if (q && q.page) {
+    const { page } = q;
+    if (page.size) {
+      if (Number(q["page[size]"]) < 0)
+        throw new BadRequestError("page[size] must be a positive integer");
+      pageSize = page.size;
+    }
+  }
+
+  const query = `select key from ${TABLE_KEY_VALUE_STORAGE} limit ?`;
+  const params = [pageSize];
+
+  const result = await cassandra.execute(query, params, { prepare: true });
+
+  const items = [];
+  result.rows.forEach((r) => {
+    items.push(r.key);
+  });
+
+  if (result.rows.length === 0)
+    return {
+      items,
+      total: 0,
+    };
+
+  const links = {
+    first: buildLink(store, null, null, pageSize),
+    prev: buildLink(store, null, null, pageSize),
+    next: buildLink(store, null, null, pageSize),
+    last: buildLink(store, null, null, pageSize),
+  };
+
+  return {
+    items,
+    total: items.length,
+    pageSize,
+    links,
+  };
+}
+
+module.exports = {
+  setKey,
+  getKey,
+  deleteKey,
+  patchKey,
+  getListKeys,
+};
--- api/key-value-storage/router.js
+++ api/key-value-storage/router.js
@@ -1,29 +1,66 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const auth = require("../../auth");
-// const logger = require("../../logger");
-// const controller = require("./controller");
+const controller = require("./controller");
 
 const router = express.Router();
 
 router.use(bodyParser.json({ limit: "10mb", extended: true, type: "*/*" }));
 
-router.use(auth.handleToken);
-
 // List of key-values
-// router.get("/", async (req, res, next) => {});
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await controller.getListKeys(req.store, req.query);
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Add or update a value
-// router.put("/:key", async (req, res, next) => {});
+router.put("/:key", async (req, res, next) => {
+  try {
+    const { key } = req.params;
+    const value = req.body;
+    const { type, result } = await controller.setKey(req.store, key, value);
+    const status = type === "insert" ? 200 : 201;
+    res.status(status).send(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Get object by key
-// router.get("/:key", async (req, res, next) => {});
+router.get("/:key", async (req, res, next) => {
+  try {
+    const { key } = req.params;
+    const result = await controller.getKey(req.store, key);
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Delete object by key
-// router.delete("/:key", async (req, res, next) => {});
+router.delete("/:key", async (req, res, next) => {
+  try {
+    const { key } = req.params;
+    await controller.deletetKey(req.store, key);
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Apply a patch or add an item to an existing value
-// router.patch("/:key", async (req, res, next) => {});
+router.patch("/:key", async (req, res, next) => {
+  try {
+    const { key } = req.params;
+    const result = await controller.patchKey(req.store, key, req.body);
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
