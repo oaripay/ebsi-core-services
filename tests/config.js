69a202192ceae1b62935720bbc7f7bd405d31e09
Julian GONZALEZ AGUDELO <Julian.GONZALEZ-AGUDELO@ext.ec.europa.eu>
2020-04-27 13:19:44 +0200

    fix/EBSIINT-710: tests e2e file storage

---
+++ tests/config.js
@@ -0,0 +1,36 @@
+const utils = require("../src/utils");
+require("dotenv").config();
+
+const port = process.env.PORT || 8080;
+
+const config = {
+  production: {
+    url: "https://api.ebsi.tech.ec.europa.eu",
+  },
+  development: {
+    url: "https://api.ebsi.xyz",
+  },
+  integration: {
+    url: "https://api.intebsi.xyz",
+  },
+  local: {
+    url: `http://localhost:${port}`,
+  },
+};
+
+if (!process.env.EBSI_ENV) throw new Error("EBSI_ENV is not defined");
+if (!process.env.TEST_APP_NAME) throw new Error("TEST_APP_NAME is not defined");
+if (!process.env.TEST_APP_PRIVATE_KEY)
+  throw new Error("TEST_APP_PRIVATE_KEY is not defined");
+
+const environment = process.env.EBSI_ENV;
+const finalConfig = config[environment];
+const { TEST_APP_NAME } = process.env;
+const privKey = utils.getJWKfromHex(process.env.TEST_APP_PRIVATE_KEY);
+const api = `${finalConfig.url}/storage/v1`;
+
+module.exports = {
+  api,
+  TEST_APP_NAME,
+  privKey,
+};
