37f96d85bb5e1550e2e5627e155cd563dce6bad1
Julian GONZALEZ AGUDELO <Julian.GONZALEZ-AGUDELO@ext.ec.europa.eu>
2020-04-30 12:32:23 +0000

    EBSIINT-1184: server as class, start function

--- api/besu/server.js
+++ api/besu/server.js
@@ -1,35 +1,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const config = require("./config");
 const logger = require("./logger");
 const auth = require("./auth");
 const besuAPI = require("./router");
 const errors = require("./errors");
 
-const app = express();
+class App {
+  constructor() {
+    this.httpServer = express();
 
-app.use("*", require("cors")());
+    this.httpServer.use("*", cors());
 
-app.use(bodyParser.json({ limit: "10mb", extended: true, type: "*/*" }));
+    this.httpServer.use(
+      bodyParser.json({ limit: "10mb", extended: true, type: "*/*" })
+    );
 
-app.use((req, res, next) => {
-  logger.info(`${req.method} ${req.url}`);
-  next();
-});
+    this.httpServer.use((req, res, next) => {
+      logger.info(`${req.method} ${req.url}`);
+      next();
+    });
 
-app.post("/ledger/v1/sessions", auth.callNewSession);
-app.use("/ledger/v1/blockchains/besu", besuAPI);
+    this.httpServer.post("/ledger/v1/sessions", auth.callNewSession);
+    this.httpServer.use("/ledger/v1/blockchains/besu", besuAPI);
 
-app.use((req, res, next) => {
-  next(new errors.BadRequestError(`Invalid service '${req.url}'`));
-});
+    this.httpServer.use((req, res, next) => {
+      next(new errors.BadRequestError(`Invalid service '${req.url}'`));
+    });
 
-app.use(errors.handler);
+    this.httpServer.use(errors.handler);
+  }
 
-const server = app.listen(config.port, () => {
-  logger.info(`Hyperledger Besu API started at port ${config.port}`);
-  if (config.testMode) logger.info("EBSI TEST MODE enabled");
-});
+  start(port, testMode = false) {
+    return this.httpServer.listen(port, () => {
+      logger.info(`Hyperledger Besu API started at port ${port}`);
+      if (testMode) logger.info("EBSI TEST MODE enabled");
+    });
+  }
+}
 
-module.exports = server;
+module.exports = App;
