8546d3e1075d09cdc2abe9355d1982b5f47ce701
Jara Quintana Zazurca <jquintza@everis.com>
2020-06-08 14:52:58 +0200

    fix testMatch

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: "node",
   rootDir: ".",
   roots: ["<rootDir>/src/", "<rootDir>/tests/"],
-  testMatch: ["**/?(*.)+(spec|test).+(ts|tsx|js)"],
+  testMatch: ["**/?(*.|*-)+(spec|test).+(ts|tsx|js)"],
   transform: {
     "^.+\\.(ts|tsx)?$": "ts-jest"
   },
