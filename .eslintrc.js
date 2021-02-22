fa7173ef93bd3a13464c4af3eb1bd2b3fb2b1b33
Benjamin MATEO <Benjamin.MATEO@ext.ec.europa.eu>
2021-02-22 12:32:37 +0100

    feat: add-common-SC (#2) - EBSIINT-2757

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  extends: [
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+  ],
+  globals: {
+    // truffle globals
+    artifacts: true,
+    contract: true,
+    assert: true,
+    web3: true,
+  },
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+  },
+  ignorePatterns: ["**/contracts/bootstrap-ethereum-sc"],
+  rules: {
+    // we use it for scripts
+    "no-console": "off",
+    // we use it for tests
+    "import/no-extraneous-dependencies": "off",
+  },
+};
