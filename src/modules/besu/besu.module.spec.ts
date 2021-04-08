84a45917bac44835b1cc2458e33dbeabe499c4a0
Yannick HUARD <huard.yan@gmail.com>
2021-04-08 14:24:15 +0200

    fix: revert depedencies update (#35) - EBSIINT-2947

--- src/modules/besu/besu.module.spec.ts
+++ src/modules/besu/besu.module.spec.ts
@@ -237,7 +237,8 @@ describe("Besu Module", () => {
       });
 
     expect(response.body).toStrictEqual({
-      detail: "Error parsing the transaction: Invalid character",
+      detail:
+        "Error parsing the transaction: Invalid chain id. Please set chain id to 0x539",
       status: 400,
       title: "Bad Request",
       type: "about:blank",
