8452c1ee868e146f4ca147a1ff621296e2fb46c3
Yannick HUARD <huard.yan@gmail.com>
2021-06-04 12:35:27 +0200

    fix: handle local API 404 response (#34) - EBSIINT-3039

--- tests/k6/script.js
+++ tests/k6/script.js
@@ -1,17 +1,15 @@
 import http from "k6/http";
 import { group, check } from "k6";
 
-const hostname = __ENV.API_HOSTNAME || "0.0.0.0";
-
-const BASE_URL = `http://${hostname}:3000`;
+const BASE_URL = __ENV.BASE_URL || "http://0.0.0.0:3000";
 
 export const options = {
   max_vus: 100,
   vus: 100,
   stages: [
-    { duration: "30s", target: 10 },
-    { duration: "4m", target: 100 },
-    { duration: "30s", target: 0 },
+    { duration: "15s", target: 10 },
+    { duration: "2m", target: 100 },
+    { duration: "15s", target: 0 },
   ],
 };
 
