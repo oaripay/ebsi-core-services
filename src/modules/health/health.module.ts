be4f8cae8c465b4c2dab966945f79ff9b2c57b86
Yannick HUARD <huard.yan@gmail.com>
2021-09-09 09:58:57 +0200

    chore: upgrade Node.js to v14.17.6 and update dependencies (#92) - EBSIINT-3284

--- src/modules/health/health.module.ts
+++ src/modules/health/health.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { HttpModule } from "@nestjs/axios";
 import { TerminusModule } from "@nestjs/terminus";
 import { ConfigService } from "@nestjs/config";
 import { ApiConfigModule } from "../../config/configuration";
 import { HealthController } from "./health.controller";
 
 @Module({
-  imports: [ApiConfigModule, TerminusModule],
+  imports: [ApiConfigModule, TerminusModule, HttpModule],
   controllers: [HealthController],
   providers: [ConfigService],
 })
