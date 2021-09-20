0f6b2cb5530cf2197cbe98dd2f7356a39877e4d2
Stefan Bancu <Stefan>
2021-09-20 20:08:29 +0300

    fixed hash algo values

--- tasks/addHashAlgo.ts
+++ tasks/addHashAlgo.ts
@@ -1,25 +1,30 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
-import {DidRegistry, Tir} from "../src/types";
-import {ethers} from "hardhat";
-
+import { ethers } from "hardhat";
+import { DidRegistry, Tir } from "../src/types";
 
 // follows ETH/BTC's BIP 39 protocol
 // https://iancoleman.io/bip39/
 // and matches the one hardhat uses when using { accounts: { mnemonic }}
-task("addHashAlgo", "Add alg hash", async (taskArgs: {proxy: string, contract: string}, {ethers}) => {
+task(
+  "addHashAlgo",
+  "Add alg hash",
+  async (taskArgs: { proxy: string; contract: string }, { ethers }) => {
+    const [deployer, admin] = await ethers.getSigners();
+    const ts = await ethers.getContractAt(
+      taskArgs.contract,
+      taskArgs.proxy,
+      admin
+    );
 
-  const [deployer, admin] = await ethers.getSigners();
-  const ts = await ethers.getContractAt(taskArgs.contract, taskArgs.proxy, admin);
-
-  console.log(
-    `deployer:${deployer.address}
+    console.log(
+      `deployer:${deployer.address}
      admin:${admin.address}`
-  );
-  const initialVersion = await ts.version();
-  console.log(initialVersion);
-  console.log("initialVersion:", initialVersion.toString());
-  /**
+    );
+    const initialVersion = await ts.version();
+    console.log(initialVersion);
+    console.log("initialVersion:", initialVersion.toString());
+    /**
    * SHA-256	1	256 bits	SHA-256	2.16.840.1.101.3.4.2.1
    https://www.iana.org/assignments/named-information/named-information.xhtml
    http://oid-info.com/cgi-bin/display?oid=2.16.840.1.101.3.4.2.1&action=display
@@ -45,152 +50,201 @@ task("addHashAlgo", "Add alg hash", async (taskArgs: {proxy: string, contract: s
    https://www.iana.org/assignments/named-information/named-information.xhtml
    http://oid-info.com/get/2.16.840.1.101.3.4.2.10
    */
-  // add hashAlgo
-  console.log("alg sha256");
-  try {
-    await ts.getHashAlgorithmById(1);
-    console.log("updating");
-    // update version
-    await (
-      await ts.updateHashAlgorithm(
-        1,
-        256,
-        "SHA256",
-        "2.16.840.1.101.3.4.2.1",
-        1
-      )
-    ).wait(1);
-  } catch (e) {
-    console.log("inserting");
-    await (
-      await ts.insertHashAlgorithm(256, "SHA256", "2.16.840.1.101.3.4.2.1", 1)
-    ).wait(1);
-  }
-  console.log("alg sha384");
-  try {
-    await ts.getHashAlgorithmById(2);
-    console.log("updating");
-    await (
-      await ts.updateHashAlgorithm(
-        2,
-        384,
-        "SHA384",
-        "2.16.840.1.101.3.4.2.2",
-        1
-      )
-    ).wait(1);
-  } catch (e) {
-    console.log("inserting");
-    await (
-      await ts.insertHashAlgorithm(384, "SHA384", "2.16.840.1.101.3.4.2.2", 1)
-    ).wait(1);
-  }
-  console.log("alg sha512");
-  try {
-    await ts.getHashAlgorithmById(3);
-    console.log("updating");
-    await (
-      await ts.updateHashAlgorithm(
-        3,
-        512,
-        "SHA512",
-        "2.16.840.1.101.3.4.2.3",
-        1
-      )
-    ).wait(1);
-  } catch (e) {
-    console.log("inserting");
-    await (
-      await ts.insertHashAlgorithm(512, "SHA512", "2.16.840.1.101.3.4.2.3", 1)
-    ).wait(1);
-  }
-  console.log("alg sha3224");
-  try {
-    await ts.getHashAlgorithmById(4);
-    console.log("updating");
-    await (
-      await ts.updateHashAlgorithm(
-        4,
-        224,
-        "SHA3224",
-        "2.16.840.1.101.3.4.2.7",
-        1
-      )
-    ).wait(1);
-  } catch (e) {
-    console.log("inserting");
-    await (
-      await ts.insertHashAlgorithm(224, "SHA3224", "2.16.840.1.101.3.4.2.7", 1)
-    ).wait(1);
-  }
-  console.log("alg sha3256");
-  try {
-    await ts.getHashAlgorithmById(5);
-    console.log("updating");
-    await (
-      await ts.updateHashAlgorithm(
-        5,
-        256,
-        "SHA3256",
-        "2.16.840.1.101.3.4.2.8",
-        1
-      )
-    ).wait(1);
-  } catch (e) {
-    console.log("inserting");
-    await (
-      await ts.insertHashAlgorithm(256, "SHA3256", "2.16.840.1.101.3.4.2.8", 1)
-    ).wait(1);
-  }
-  console.log("alg sha3384");
-  try {
-    await ts.getHashAlgorithmById(6);
-    console.log("updating");
-    await (
-      await ts.updateHashAlgorithm(
-        6,
-        384,
-        "SHA3384",
-        "2.16.840.1.101.3.4.2.9",
-        1
-      )
-    ).wait(1);
-  } catch (e) {
-    console.log("inserting");
-    await (
-      await ts.insertHashAlgorithm(384, "SHA3384", "2.16.840.1.101.3.4.2.9", 1)
-    ).wait(1);
-  }
-  console.log("alg sha33512");
-  try {
-    await ts.getHashAlgorithmById(2);
-    console.log("updating");
-    await (
-      await ts.updateHashAlgorithm(
-        7,
-        512,
-        "SHA3512",
-        "2.16.840.1.101.3.4.2.10",
-        1
-      )
-    ).wait(1);
-  } catch (e) {
-    console.log("inserting");
-    await (
-      await ts.insertHashAlgorithm(512, "SHA3512", "2.16.840.1.101.3.4.2.10", 1)
-    ).wait(1);
-  }
+    // add hashAlgo
+    console.log("alg sha256");
+    try {
+      await ts.getHashAlgorithmById(1);
+      console.log("updating");
+      // update version
+      await (
+        await ts.updateHashAlgorithm(
+          1,
+          256,
+          "sha-256",
+          "2.16.840.1.101.3.4.2.1",
+          1,
+          "sha2-256"
+        )
+      ).wait(1);
+    } catch (e) {
+      console.log("inserting");
+      await (
+        await ts.insertHashAlgorithm(
+          256,
+          "sha-256",
+          "2.16.840.1.101.3.4.2.1",
+          1,
+          "sha2-256"
+        )
+      ).wait(1);
+    }
+    console.log("alg sha384");
+    try {
+      await ts.getHashAlgorithmById(2);
+      console.log("updating");
+      await (
+        await ts.updateHashAlgorithm(
+          2,
+          384,
+          "sha-384",
+          "2.16.840.1.101.3.4.2.2",
+          1,
+          ""
+        )
+      ).wait(1);
+    } catch (e) {
+      console.log("inserting");
+      await (
+        await ts.insertHashAlgorithm(
+          384,
+          "sha-384",
+          "2.16.840.1.101.3.4.2.2",
+          1,
+          ""
+        )
+      ).wait(1);
+    }
+    console.log("alg sha512");
+    try {
+      await ts.getHashAlgorithmById(3);
+      console.log("updating");
+      await (
+        await ts.updateHashAlgorithm(
+          3,
+          512,
+          "sha-512",
+          "2.16.840.1.101.3.4.2.3",
+          1,
+          "sha2-512"
+        )
+      ).wait(1);
+    } catch (e) {
+      console.log("inserting");
+      await (
+        await ts.insertHashAlgorithm(
+          512,
+          "sha-512",
+          "2.16.840.1.101.3.4.2.3",
+          1,
+          "sha2-512"
+        )
+      ).wait(1);
+    }
+    console.log("alg sha3224");
+    try {
+      await ts.getHashAlgorithmById(4);
+      console.log("updating");
+      await (
+        await ts.updateHashAlgorithm(
+          4,
+          224,
+          "sha3-224",
+          "2.16.840.1.101.3.4.2.7",
+          1,
+          "sha3-224"
+        )
+      ).wait(1);
+    } catch (e) {
+      console.log("inserting");
+      await (
+        await ts.insertHashAlgorithm(
+          224,
+          "sha3-224",
+          "2.16.840.1.101.3.4.2.7",
+          1,
+          "sha3-224"
+        )
+      ).wait(1);
+    }
+    console.log("alg sha3256");
+    try {
+      await ts.getHashAlgorithmById(5);
+      console.log("updating");
+      await (
+        await ts.updateHashAlgorithm(
+          5,
+          256,
+          "sha3-256",
+          "2.16.840.1.101.3.4.2.8",
+          1,
+          "sha3-256"
+        )
+      ).wait(1);
+    } catch (e) {
+      console.log("inserting");
+      await (
+        await ts.insertHashAlgorithm(
+          256,
+          "sha3-256",
+          "2.16.840.1.101.3.4.2.8",
+          1,
+          "sha3-256"
+        )
+      ).wait(1);
+    }
+    console.log("alg sha3384");
+    try {
+      await ts.getHashAlgorithmById(6);
+      console.log("updating");
+      await (
+        await ts.updateHashAlgorithm(
+          6,
+          384,
+          "sha3-384",
+          "2.16.840.1.101.3.4.2.9",
+          1,
+          "sha3-384"
+        )
+      ).wait(1);
+    } catch (e) {
+      console.log("inserting");
+      await (
+        await ts.insertHashAlgorithm(
+          384,
+          "sha3-384",
+          "2.16.840.1.101.3.4.2.9",
+          1,
+          "sha3-384"
+        )
+      ).wait(1);
+    }
+    console.log("alg sha33512");
+    try {
+      await ts.getHashAlgorithmById(2);
+      console.log("updating");
+      await (
+        await ts.updateHashAlgorithm(
+          7,
+          512,
+          "sha3-512",
+          "2.16.840.1.101.3.4.2.10",
+          1,
+          "sha3-512"
+        )
+      ).wait(1);
+    } catch (e) {
+      console.log("inserting");
+      await (
+        await ts.insertHashAlgorithm(
+          512,
+          "sha3-512",
+          "2.16.840.1.101.3.4.2.10",
+          1,
+          "sha3-512"
+        )
+      ).wait(1);
+    }
 
-  for (let i = 1; i <= 7; i += 1) {
-    // eslint-disable-next-line no-await-in-loop
-    const halgo = await ts.getHashAlgorithmById(i);
-    console.log(
-      `halgorithm ${halgo.ianaName} oid:${
-        halgo.oid
-      } length:${halgo.outputLength.toString()} status:${halgo.status}`
-    );
+    for (let i = 1; i <= 7; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      const halgo = await ts.getHashAlgorithmById(i);
+      console.log(
+        `halgorithm ${halgo.ianaName} oid:${
+          halgo.oid
+        } length:${halgo.outputLength.toString()} status:${halgo.status}`
+      );
+    }
   }
-
-})
+)
   .addParam("proxy", "Proxy Address")
   .addParam("contract", "Contract Tag to which alg should be added");
