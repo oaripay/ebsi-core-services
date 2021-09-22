79a5386f39ce7c61a7558cb2853ee75457e45ef5
Stefan-Adrian Bancu <bancu.stefan@gmail.com>
2021-09-22 15:58:49 +0300

    split code

--- src/pages/RegisterDid/DidUtils.ts
+++ src/pages/RegisterDid/DidUtils.ts
@@ -1,3 +1,76 @@
+import { ethers } from "ethers";
+import { ec as EC } from "elliptic";
+import { EbsiWallet } from "@cef-ebsi/wallet-lib";
+import bs58 from "bs58";
+import crypto from "crypto";
+
+export function fromHexString(hexString: string): Uint8Array {
+  const match = hexString.match(/.{1,2}/g);
+  if (!match) throw new Error("String could not be parsed");
+  return new Uint8Array(match.map((byte) => parseInt(byte, 16)));
+}
+
+export function createMetadata() {
+  return {
+    meta: crypto.randomBytes(32).toString("hex"),
+  };
+}
+
 export const getIdentifierFromWalletAddr = (walletAddress: string) => {
   return `did:ebsi:${walletAddress}`;
 };
+
+export function computeIdentifier(did: string): string {
+  return `0x${Buffer.from(did).toString("hex")}`;
+}
+
+export function createTimestamp() {
+  return {
+    data: crypto.randomBytes(32).toString("hex"),
+  };
+}
+
+export function buildDidParams(document: any) {
+  const bufferTimestamp = Buffer.from(JSON.stringify(createTimestamp()));
+  const bufferDocument = Buffer.from(JSON.stringify(document));
+  const bufferMetadata = Buffer.from(JSON.stringify(createMetadata()));
+  const documentHash = ethers.utils.sha256(bufferDocument);
+  return {
+    info: {
+      title: "Did document",
+      data: document,
+    },
+    param: {
+      identifier: computeIdentifier(document.id),
+      hashAlgorithmId: 1, // sha256
+      hashValue: documentHash,
+      didVersionInfo: `0x${bufferDocument.toString("hex")}`,
+      timestampData: `0x${bufferTimestamp.toString("hex")}`,
+      didVersionMetadata: `0x${bufferMetadata.toString("hex")}`,
+    },
+  };
+}
+
+export function createDidDocument(didUser: string, publicKey: string) {
+  const ec = new EC("secp256k1");
+  const key = ec.keyFromPublic(publicKey.slice(2), "hex");
+  const publicKeyObj = {
+    publicKeyHex: publicKey.slice(2),
+    publicKeyJwk: EbsiWallet.formatPublicKey(key.getPublic(), "jwk"),
+    publicKeyBase58: bs58.encode(fromHexString(publicKey.slice(2))),
+  };
+  return {
+    "@context": "https://w3id.org/did/v1",
+    id: didUser,
+    verificationMethod: [
+      {
+        id: `${didUser}#keys-1`,
+        type: "Secp256k1VerificationKey2018",
+        controller: didUser,
+        ...publicKeyObj,
+      },
+    ],
+    authentication: [`${didUser}#keys-1`],
+    assertionMethod: [`${didUser}#keys-1`],
+  };
+}
--- src/pages/RegisterDid/use-did-register.ts
+++ src/pages/RegisterDid/use-did-register.ts
@@ -1,83 +1,15 @@
-import * as crypto from "crypto";
-import { ec as EC } from "elliptic";
-import bs58 from "bs58";
-
-import { EbsiWallet } from "@cef-ebsi/wallet-lib";
 import { ethers } from "ethers";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { notification } from "antd";
 import { useEthersHook } from "../../hooks/use-ethers.hook";
 import { useWalletContext } from "../../components/Wallet/WalletContext";
-import { getIdentifierFromWalletAddr } from "./DidUtils";
+import {
+  buildDidParams,
+  createDidDocument,
+  getIdentifierFromWalletAddr,
+} from "./DidUtils";
 
 type DidRecordDataType = {};
-
-export function createMetadata() {
-  return {
-    meta: crypto.randomBytes(32).toString("hex"),
-  };
-}
-
-export function createTimestamp() {
-  return {
-    data: crypto.randomBytes(32).toString("hex"),
-  };
-}
-
-export function fromHexString(hexString: string): Uint8Array {
-  const match = hexString.match(/.{1,2}/g);
-  if (!match) throw new Error("String could not be parsed");
-  return new Uint8Array(match.map((byte) => parseInt(byte, 16)));
-}
-
-export function createDidDocument(didUser: string, publicKey: string) {
-  const ec = new EC("secp256k1");
-  const key = ec.keyFromPublic(publicKey.slice(2), "hex");
-  const publicKeyObj = {
-    publicKeyHex: publicKey.slice(2),
-    publicKeyJwk: EbsiWallet.formatPublicKey(key.getPublic(), "jwk"),
-    publicKeyBase58: bs58.encode(fromHexString(publicKey.slice(2))),
-  };
-  return {
-    "@context": "https://w3id.org/did/v1",
-    id: didUser,
-    verificationMethod: [
-      {
-        id: `${didUser}#keys-1`,
-        type: "Secp256k1VerificationKey2018",
-        controller: didUser,
-        ...publicKeyObj,
-      },
-    ],
-    authentication: [`${didUser}#keys-1`],
-    assertionMethod: [`${didUser}#keys-1`],
-  };
-}
-
-export function computeIdentifier(did: string): string {
-  return `0x${Buffer.from(did).toString("hex")}`;
-}
-
-export function buildDidParams(document: any) {
-  const bufferTimestamp = Buffer.from(JSON.stringify(createTimestamp()));
-  const bufferDocument = Buffer.from(JSON.stringify(document));
-  const bufferMetadata = Buffer.from(JSON.stringify(createMetadata()));
-  const documentHash = ethers.utils.sha256(bufferDocument);
-  return {
-    info: {
-      title: "Did document",
-      data: document,
-    },
-    param: {
-      identifier: computeIdentifier(document.id),
-      hashAlgorithmId: 1, // sha256
-      hashValue: documentHash,
-      didVersionInfo: `0x${bufferDocument.toString("hex")}`,
-      timestampData: `0x${bufferTimestamp.toString("hex")}`,
-      didVersionMetadata: `0x${bufferMetadata.toString("hex")}`,
-    },
-  };
-}
 
 export default function useDidRegister() {
   const { didRegistryContract, registryContract } = useEthersHook();
