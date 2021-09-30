6ff0b3ee012c3a320fec430a051197c809800430
Stefan-Adrian Bancu <bancu.stefan@gmail.com>
2021-09-30 13:02:56 +0300

    code review changes

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,12 @@
 export const config = {
   PAGE_SIZE: 50,
   EBSI_CHAIN_ID: process.env.REACT_APP_EBSI_CHAIN_ID || 6175,
-  PROVIDER:
-    process.env.REACT_APP_PROVIDER ||
-    "https://ebsi:password@www.test.intebsi.xyz/besu",
   REGISTRY_ADDRESS:
     process.env.REACT_APP_REGISTRY_ADDRESS ||
     "0x4d06b562588cb61616959806726c5d9f060b0f21",
   DID_REGISTRY_ADDRESS:
     process.env.REACT_APP_DID_REGISTRY_ADDRESS ||
     "0x15582f47140ff4bd74843583a1e3111032fb91c8",
-  NOTIFICATION_URL:
-    process.env.REACT_APP_TAW_TX_URI || "/raw-transaction-signatures",
-  REDIRECT_URL: process.env.REACT_APP_REDIRECT_URL || "http://localhost:3000",
-  WALLET_WEB_CLIENT_URL:
-    process.env.REACT_APP_WALLET_WEB_CLIENT_URL ||
-    "https://app.intebsi.xyz/wallet/notifications",
-  EBSI_API:
-    process.env.REACT_APP_WALLET_API || "https://api.intebsi.xyz/wallet/v1",
   routes: {
     trustedAppsRegistry: "/apps-admin/trusted-apps-registry",
     registerDid: "/apps-admin/register-did",
