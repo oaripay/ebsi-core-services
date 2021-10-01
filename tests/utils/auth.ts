a62abe87f1d9423fed496109c380a7d3d0f621a9
Yannick HUARD <huard.yan@gmail.com>
2021-10-01 08:59:48 +0200

    feat: support DID JWT with publicKeyMultibase (#81) - EBSIINT-3332

--- tests/utils/auth.ts
+++ tests/utils/auth.ts
@@ -2,9 +2,8 @@
 import request from "supertest";
 import crypto from "crypto";
 import { v4 as uuidv4 } from "uuid";
-import { EbsiDidAuth, DidAuthResponseMode, Agent } from "@cef-ebsi/siop-auth";
+import { DidAuthResponseMode, Agent } from "@cef-ebsi/siop-auth";
 import { AkeResponse, Agent as OAuth2Agent } from "@cef-ebsi/oauth2-auth";
-import querystring from "querystring";
 import { loadConfig } from "../../src/config/configuration";
 import { prefixWith0x } from "../../src/shared/utils";
 
@@ -50,25 +49,29 @@ export async function siopAuthentication(user: {
   privateKey: string;
   did: string;
 }): Promise<string> {
+  const agent = new Agent({
+    privateKey: prefixWith0x(user.privateKey),
+    didRegistry: `${didRegistryApiUrl}/identifiers`,
+  });
+
   let response = await request(authorisationApiUrl)
     .post("/authentication-requests")
     .send({
       scope: "openid did_authn",
     });
-  const uriDecoded = querystring.decode(
+
+  const uriDecoded = new URLSearchParams(
     (response.body as { uri: string }).uri.replace("openid://?", "")
-  ) as { request: string; client_id: string };
-  await EbsiDidAuth.verifyAuthenticationRequest(
-    uriDecoded.request,
-    `${didRegistryApiUrl}/identifiers`
   );
 
+  await agent.verifyAuthenticationRequest(uriDecoded.get("request"));
+
   const nonce = crypto.randomBytes(10).toString("base64");
-  const didAuthJwt = await EbsiDidAuth.createAuthenticationResponse({
-    hexPrivateKey: prefixWith0x(user.privateKey),
+
+  const didAuthJwt = await agent.createAuthenticationResponse({
     did: user.did,
     nonce,
-    redirectUri: uriDecoded.client_id,
+    redirectUri: uriDecoded.get("client_id"),
     responseMode: DidAuthResponseMode.FORM_POST,
   });
 
@@ -76,10 +79,6 @@ export async function siopAuthentication(user: {
     .post("/siop-sessions")
     .send(didAuthJwt.bodyEncoded);
 
-  const agent = new Agent({
-    privateKey: user.privateKey,
-    didRegistry: `${didRegistryApiUrl}/identifiers`,
-  });
   const accessToken = await agent.verifyAuthenticationResponse(
     response.body,
     nonce
