67670a0e349a750b6da035377b22e890a2618e3e
Yannick HUARD <huard.yan@gmail.com>
2021-10-01 12:58:35 +0200

    feat: support DID JWT with publicKeyMultibase (#43) - EBSIINT-3363

--- tests/utils/siopJwt.ts
+++ tests/utils/siopJwt.ts
@@ -1,11 +1,6 @@
-import querystring from "querystring";
-import axios from "axios";
-import {
-  Agent as SiopAgent,
-  DidAuthResponseMode,
-  AkeResponse,
-} from "@cef-ebsi/siop-auth";
+import request from "supertest";
 import { randomUUID } from "crypto";
+import { Agent as SiopAgent, DidAuthResponseMode } from "@cef-ebsi/siop-auth";
 
 export const requestSiopJwt = async ({
   didRegistry,
@@ -18,60 +13,43 @@ export const requestSiopJwt = async ({
   clientPrivateKey: string;
   authorisationApiUrl: string;
 }): Promise<string> => {
-  // 0. Create agent
   const siopAgent = new SiopAgent({
     privateKey: `0x${clientPrivateKey}`,
     didRegistry,
   });
 
   // 1. First, the client calls /authentication-requests
-  const authenticationRequestsResponse = await axios.post<{ uri: string }>(
-    `${authorisationApiUrl}/authentication-requests`,
-    {
+  let response = await request(authorisationApiUrl)
+    .post("/authentication-requests")
+    .send({
       scope: "openid did_authn",
-    }
-  );
+    });
 
   // 2. The client verifies the response
-  const { uri } = authenticationRequestsResponse.data;
-  const uriDecoded = querystring.decode(uri.replace("openid://?", "")) as {
-    request: string;
-  };
-
-  const payload = await siopAgent.verifyAuthenticationRequest(
-    uriDecoded.request
+  const uriDecoded = new URLSearchParams(
+    (response.body as { uri: string }).uri.replace("openid://?", "")
   );
+
+  await siopAgent.verifyAuthenticationRequest(uriDecoded.get("request"));
 
   // 3. The client creates an authentication response and gets an ID Token
   const nonce = randomUUID();
 
-  const authenticationResponse = await siopAgent.createAuthenticationResponse({
+  const didAuthJwt = await siopAgent.createAuthenticationResponse({
     did: clientDid,
     nonce,
-    redirectUri: payload.client_id,
+    redirectUri: uriDecoded.get("client_id"),
     responseMode: DidAuthResponseMode.FORM_POST,
   });
 
-  const authResponseDecoded = querystring.decode(
-    authenticationResponse.bodyEncoded ?? ""
-  );
-
-  const idToken = authResponseDecoded.id_token;
-
   // 4. The client call /siop-sessions with the ID Token
-  const siopSessionsResponse = await axios.post<AkeResponse>(
-    `${authorisationApiUrl}/siop-sessions`,
-    querystring.stringify({ id_token: idToken }),
-    {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    }
-  );
+  response = await request(authorisationApiUrl)
+    .post("/siop-sessions")
+    .send(didAuthJwt.bodyEncoded);
 
   // 5. Finally, the client verifies the SIOP authentication response and gets an access token
   const accessToken = await siopAgent.verifyAuthenticationResponse(
-    siopSessionsResponse.data,
+    response.body,
     nonce
   );
 
