df6614fb00f777f975f1e19ca295edfa62e0131a
Yannick HUARD <Yannick.HUARD@ext.ec.europa.eu>
2021-11-05 17:29:09 +0100

    test: fix e2e test (#82)

--- tests/e2e/app.e2e-spec.ts
+++ tests/e2e/app.e2e-spec.ts
@@ -92,7 +92,7 @@ describe("TAR API (generic tests)", () => {
       const response = await request(app.getHttpServer())
         .post("/jsonrpc")
         .auth(
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJpc3MiOiJhbnkifQ.eiwf-6rtNV0oWpFidRTlcY6oLBpV0l2tEkCs5FNoIxY",
           { type: "bearer" }
         )
         .send();
