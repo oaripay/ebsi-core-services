e97826cc641a044f7fd41f6e783169733e77b465
Yannick HUARD <Yannick.HUARD@ext.ec.europa.eu>
2021-12-16 17:21:31 +0100

    feat: log requests and responses (#83) - EBSIINT-3651
    Merge in EBSI/trusted-issuers-registry-api from feat/EBSIINT-3651 to develop

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module, Logger } from "@nestjs/common";
 import { TerminusModule } from "@nestjs/terminus";
+import { APP_INTERCEPTOR } from "@nestjs/core";
 import { ApiConfigModule } from "./config/configuration";
 import { AdministratorsModule } from "./modules/administrators/administrators.module";
 import { IssuersModule } from "./modules/issuers/issuers.module";
 import { JsonRpcModule } from "./modules/jsonrpc/jsonrpc.module";
 import { PoliciesModule } from "./modules/policies/policies.module";
 import { HealthModule } from "./modules/health/health.module";
+import { LoggingInterceptor } from "./interceptors/logging.interceptor";
 
 @Module({
   imports: [
@@ -18,7 +20,13 @@ import { HealthModule } from "./modules/health/health.module";
     HealthModule,
   ],
   controllers: [],
-  providers: [Logger],
+  providers: [
+    Logger,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
 
