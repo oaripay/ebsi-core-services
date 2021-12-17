58dc1b3743ed844af9c2d4e9efa6606244b35d73
Yannick HUARD <Yannick.HUARD@ext.ec.europa.eu>
2021-12-17 08:44:40 +0100

    feat: log requests and responses (#57) - EBSIINT-3651
    Merge in EBSI/trusted-schemas-registry-api from feat/EBSIINT-3651 to develop

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
+import { APP_INTERCEPTOR } from "@nestjs/core";
 import { ApiConfigModule } from "./config/configuration";
 import { HealthModule } from "./modules/health/health.module";
 import { JsonRpcModule } from "./modules/jsonrpc/jsonrpc.module";
 import { SchemasModule } from "./modules/schemas/schemas.module";
 import { AdministratorsModule } from "./modules/administrators/administrators.module";
 import { PoliciesModule } from "./modules/policies/policies.module";
+import { LoggingInterceptor } from "./interceptors/logging.interceptor";
 
 @Module({
   imports: [
@@ -15,7 +17,12 @@ import { PoliciesModule } from "./modules/policies/policies.module";
     AdministratorsModule,
     PoliciesModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
 
