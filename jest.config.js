4c0d6358a521cb0ab2e0541e95dcb747250a12a5
Mauro Lucchini <mauro.lucchini@validatedid.com>
2022-01-04 16:14:09 +0100

    chore: update axios, ebsi libs and minors - EBSIINT-3710

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,4 @@ module.exports = {
   coverageDirectory: "./coverage/",
   collectCoverageFrom: ["src/**/*.(t|j)s", "!**/*.d.ts", "!src/main.ts"],
   coverageReporters: ["text", "lcov", "json", "clover", "cobertura"],
-  moduleNameMapper: {
-    "^jose/(.*)$": "<rootDir>/node_modules/jose/dist/node/cjs/$1",
-  },
 };
