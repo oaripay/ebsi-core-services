17f9025ea4fa6be9ceb018ea49bc04a3b4f91a71
Yannick HUARD <Yannick.HUARD@ext.ec.europa.eu>
2022-01-06 11:07:49 +0100

    chore: update dependencies (#55) - EBSIINT-3730
    Merge in EBSI/notifications-api from chore/EBSIINT-3730 to develop

--- tests/utils/auth.ts
+++ tests/utils/auth.ts
@@ -1,5 +1,6 @@
+import { URLSearchParams } from "node:url";
+import crypto from "node:crypto";
 import request from "supertest";
-import crypto from "crypto";
 import { EbsiWallet } from "@cef-ebsi/wallet-lib";
 import { DidAuthResponseMode, Agent, AkeResponse } from "@cef-ebsi/siop-auth";
 import { loadConfig } from "../../src/config/configuration";
