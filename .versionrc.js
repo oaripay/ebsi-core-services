ad80c4a9acf122fdb8da4aaf5991270b40f474af
Yannick HUARD <Yannick.HUARD@ext.ec.europa.eu>
2022-03-07 14:22:42 +0100

    docs: enhance OpenAPI definition (#70) - EBSIINT-3903
    Merge in EBSI/users-onboarding-api from docs/EBSIINT-3903 to develop

--- .versionrc.js
+++ .versionrc.js
@@ -16,9 +16,9 @@ module.exports = {
     { type: "test", hidden: true },
   ],
   commitUrlFormat:
-    "https://ec.europa.eu/cefdigital/code/projects/EBSI/repos/users-onboarding-api/commits/{{hash}}",
+    "https://ec.europa.eu/digital-building-blocks/code/projects/EBSI/repos/users-onboarding-api/commits/{{hash}}",
   issueUrlFormat:
-    "https://ec.europa.eu/cefdigital/code/projects/EBSI/repos/users-onboarding-api/pull-requests/{{id}}/overview",
+    "https://ec.europa.eu/digital-building-blocks/code/projects/EBSI/repos/users-onboarding-api/pull-requests/{{id}}/overview",
   compareUrlFormat:
-    "https://ec.europa.eu/cefdigital/code/projects/EBSI/repos/users-onboarding-api/compare/diff?targetBranch=refs%2Ftags%2F{{previousTag}}&sourceBranch=refs%2Ftags%2F{{currentTag}}&targetRepoId=234",
+    "https://ec.europa.eu/digital-building-blocks/code/projects/EBSI/repos/users-onboarding-api/compare/diff?targetBranch=refs%2Ftags%2F{{previousTag}}&sourceBranch=refs%2Ftags%2F{{currentTag}}&targetRepoId=234",
 };
