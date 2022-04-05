36eeb9fc4fc430e5809020daddd258dc89699492
Yannick HUARD <Yannick.HUARD@ext.ec.europa.eu>
2022-04-05 14:42:06 +0200

    fix: update VC lib, support latest APIs (#97) - EBSIINT-4037
    Merge in EBSI/authorisation-api from fix/EBSIINT-4037 to develop

--- tests/utils/verfiablePresentation.ts
+++ tests/utils/verfiablePresentation.ts
@@ -5,7 +5,6 @@ import type {
   EbsiVerifiablePresentation,
 } from "@cef-ebsi/verifiable-presentation";
 import { ES256KSigner, Signer, EdDSASigner } from "did-jwt";
-import { EbsiEnvConfiguration } from "@cef-ebsi/verifiable-credential";
 
 export async function createVpJwt(
   holderDid: string,
@@ -43,21 +42,6 @@ export async function createVpJwt(
     {
       ebsiEnv,
       skipValidation: true,
-      // todo: remove ebsiEnvConfig after libraries are updated
-      ...(ebsiEnv === "test" && {
-        ebsiEnvConfig: {
-          didRegistry:
-            "https://api.test.intebsi.xyz/did-registry/v3/identifiers",
-          trustedIssuersRegistry:
-            "https://api.test.intebsi.xyz/trusted-issuers-registry/v3/issuers",
-          trustedSchemasRegistry:
-            "https://api.test.intebsi.xyz/trusted-schemas-registry/v2/schemas",
-          ebsiVerifiableAttestationSchemaUrl:
-            "https://api.test.intebsi.xyz/trusted-schemas-registry/v1/schemas/0x28d76954924d1c4747a4f1f9e3e9edc9ca965efbf8ff20e4339c2bf2323a5773",
-          ebsiVerifiablePresentationSchemaUrl:
-            "https://api.test.intebsi.xyz/trusted-schemas-registry/v2/schemas/0x7e53dd8c85ffdbf0fff15599e887d6221ab02982d24d857aae1b3d2cc294f048",
-        } as EbsiEnvConfiguration,
-      }),
     }
   );
 
