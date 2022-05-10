5bb6a49c38e6d25f3f91a50eaafa4f90465380b9
Stefan Bancu <Stefan>
2022-05-10 14:45:54 +0300

    fix: added tpr address

--- contracts/timestamp/TimestampDetailed.sol
+++ contracts/timestamp/TimestampDetailed.sol
@@ -41,6 +41,9 @@ contract TimestampDetailed is Initializable, TimestampStorage {
         } else if (id == 7176) {
             // SBSI TPR Address
             tprAddress = 0x88aaea75E5D6965B526Cf2D940De22f4Ee314760;
+        } else if (id == 6177) {
+            // SBSI TPR Address
+            tprAddress = 0x18B271cCb08704d0F819284637225e31fF0B5EA9;
         }
         hs.trustedPolicyRegistry = IPolicyRegistry(tprAddress);
     }
