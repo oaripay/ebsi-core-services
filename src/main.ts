620018ffc7cd8792a10ce92dae77c9ae8fb1259d
Yannick HUARD <Yannick.HUARD@ext.ec.europa.eu>
2022-06-07 13:57:31 +0200

    chore: update fastify helmt (#105) - EBSIINT-4225
    Merge in EBSI/authorisation-api from chore/EBSIINT-4225 to develop

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import {
 } from "@nestjs/platform-fastify";
 import { ConfigService } from "@nestjs/config";
 import { ValidationPipe } from "@nestjs/common";
-import { fastifyHelmet } from "fastify-helmet";
+import { fastifyHelmet } from "@fastify/helmet";
 import { AppModule } from "./app.module";
 import { AllExceptionsFilter } from "./filters/http-exception.filter";
 import { createLogger, consoleTransport } from "./logger/logger";
