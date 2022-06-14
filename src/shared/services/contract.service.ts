e1ec27ce25aa777bb0e3b91ccce3df33cc8ad40f
Kalin CHERNEV <kalin.chernev@gmail.com>
2022-06-14 13:18:14 +0200

    chore: bump ebsi deps (#81) - EBSIINT-4288
    Merge in EBSI/trusted-schemas-registry-api from chore/EBSIINT-4288 to develop

--- src/shared/services/contract.service.ts
+++ src/shared/services/contract.service.ts
@@ -4,7 +4,7 @@ import { decodeJWT } from "did-jwt";
 import { Injectable, Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { ethers } from "ethers";
-import axios, { AxiosError, AxiosResponse } from "axios";
+import axios, { AxiosResponse } from "axios";
 import { randomUUID } from "crypto";
 import { ApiConfig } from "../../config/configuration";
 import { SchemaSCRegistry, SchemaSCRegistry__factory } from "../../contracts";
@@ -78,8 +78,8 @@ export class ContractService {
       return accessToken;
     } catch (err) {
       if (err instanceof Error) {
-        if ((err as AxiosError).isAxiosError) {
-          logAxiosError(err as AxiosError, this.logger);
+        if (axios.isAxiosError(err)) {
+          logAxiosError(err, this.logger);
         } else {
           this.logger.error(err.message, err.stack);
         }
