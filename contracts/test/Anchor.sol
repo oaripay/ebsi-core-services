50cb0fb4dafbdfb8ada25b19a9dfafa0bd0f3d27
Stefan-Adrian Bancu <bancu.stefan@gmail.com>
2022-06-29 10:48:12 +0200

    Pull request #7: chore: compiler version bump
    Merge in EBSI/proxy-ethereum-sc from EBSISD-1607 to develop

--- contracts/test/Anchor.sol
+++ contracts/test/Anchor.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: EUPL V1.2
-pragma solidity ^0.8.0;
+pragma solidity 0.8.12;
 
 import "../bootstrap-ethereum-sc/contracts/utils/upgradeability/Initializable.sol";
 import "../bootstrap-ethereum-sc/contracts/utils/pausable/Pausable.sol";
