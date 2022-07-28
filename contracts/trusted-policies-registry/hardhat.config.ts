a0418a199183b6b6013f166a781270657ed3107b
Kalin Chernev <kalin.chernev@gmail.com>
2022-07-28 14:16:24 +0200

    chore: unify typechain dependencies

--- contracts/trusted-policies-registry/hardhat.config.ts
+++ contracts/trusted-policies-registry/hardhat.config.ts
@@ -1,6 +1,6 @@
+import "@typechain/hardhat";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-solhint";
-import "hardhat-typechain";
 import "hardhat-abi-exporter";
 import "solidity-coverage";
 import { HardhatUserConfig, task } from "hardhat/config";
