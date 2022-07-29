1e1b9cbafb31366b44de38cccacb9214135e1c86
Stefan-Adrian Bancu <bancu.stefan@gmail.com>
2022-07-29 05:30:26 +0200

    Pull request #27: chore: added comments to the mock
    Merge in EBSI/did-registry-ethereum-sc from EBSISD-1687 to develop

--- contracts/mocks/DidRegistryMock.sol
+++ contracts/mocks/DidRegistryMock.sol
@@ -4,6 +4,12 @@ pragma solidity 0.8.12;
 contract DidRegistryMock {
     bool public didResult;
 
+    /**
+     * @dev check controller is owner on a did identifier, return mock result
+     * @param identifier bytes
+     * @param ctrl address
+     * @return didResult bool
+     */
     function checkController(bytes calldata identifier, address ctrl)
         external
         view
@@ -12,6 +18,10 @@ contract DidRegistryMock {
         return didResult;
     }
 
+    /**
+     * @dev set did result mock value as state variable
+     * @param newDidResult bool
+     */
     function setDidResult(bool newDidResult) external {
         didResult = newDidResult;
     }
--- contracts/mocks/PolicyRegistryMock.sol
+++ contracts/mocks/PolicyRegistryMock.sol
@@ -4,6 +4,12 @@ pragma solidity 0.8.12;
 contract PolicyRegistryMock {
     bool public policyResult;
 
+    /**
+     * @dev check policy name against user, return state variable
+     * @param policyName string
+     * @param user address
+     * @return bool policyResult
+     */
     function checkPolicy(string calldata policyName, address user)
         external
         view
@@ -12,6 +18,10 @@ contract PolicyRegistryMock {
         return policyResult;
     }
 
+    /**
+     * @dev set policy result mock value as state variable
+     * @param newPolicyResult bool
+     */
     function setPolicyResult(bool newPolicyResult) external {
         policyResult = newPolicyResult;
     }
