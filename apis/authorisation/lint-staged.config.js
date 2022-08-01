57a8788b45ba1f7aca9860264413451eb534f428
Kalin Chernev <kalin.chernev@gmail.com>
2022-08-01 17:10:24 +0200

    chore: hoist husky and commit lint

--- apis/authorisation/lint-staged.config.js
+++ apis/authorisation/lint-staged.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  "api/openapi.yaml": "openapi lint",
+  "**/openapi.yaml": "spectral lint",
   "*.{js,ts}": ["eslint --fix"],
   "*.ts": () => "tsc -p tsconfig.json --noEmit --incremental false",
   "*.{md,json,yml,yaml}": ["prettier --write"],
+  "*.sol": ["solhint"],
 };
