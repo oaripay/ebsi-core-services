de6000321877e9c6207d73e5b22a906512e0396e
Kalin Chernev <kalin.chernev@gmail.com>
2022-08-24 17:46:11 +0200

    chore: save for the day

--- apps/trusted-apps-registry/.eslintrc.js
+++ apps/trusted-apps-registry/.eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  parser: "@typescript-eslint/parser",
   root: true,
   env: {
     browser: true,
@@ -22,5 +23,12 @@ module.exports = {
     "no-restricted-syntax": 0,
     "import/extensions": 0,
     "import/no-unresolved": 0,
+    // All the below ignore rules have been added during monorepo migration and eslint deps version alignment.
+    "no-unused-vars": 0,
+    "react/jsx-no-useless-fragment": 0,
+    "no-shadow": 0,
+    "react/jsx-no-constructed-context-values": 0,
+    "no-promise-executor-return": 0,
+    "react/no-unstable-nested-components": 0,
   },
 };
