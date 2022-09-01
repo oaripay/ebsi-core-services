81ecf5271392e3f9e016ebadf20bc006b7495550
Yannick Huard <huard.yan@gmail.com>
2022-09-01 18:23:40 +0200

    test: debug tar api unit tests

--- apis/trusted-apps-registry/hardhat.config.ts
+++ apis/trusted-apps-registry/hardhat.config.ts
@@ -1,6 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import "@nomiclabs/hardhat-ethers";
 import { HardhatUserConfig } from "hardhat/config";
+import path from "path";
+
+console.error(
+  path.resolve(
+    require.resolve("@ebsiint-sc/trusted-apps-registry"),
+    "../../contracts"
+  )
+);
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -10,13 +18,27 @@ const config: HardhatUserConfig = {
     },
   },
   solidity: {
-    version: "0.8.12",
+    version: "0.8.9",
     settings: {
       optimizer: {
         enabled: true,
         runs: 10,
       },
     },
+  },
+  paths: {
+    sources: path.resolve(
+      require.resolve("@ebsiint-sc/trusted-apps-registry"),
+      "../../contracts"
+    ),
+    cache: path.resolve(
+      require.resolve("@ebsiint-sc/trusted-apps-registry"),
+      "../../cache"
+    ),
+    artifacts: path.resolve(
+      require.resolve("@ebsiint-sc/trusted-apps-registry"),
+      "../../artifacts"
+    ),
   },
 };
 
