8a39c7e0d5e00fa4cf008db24dea09ed2e3c3027
Stefan Bancu <Stefan>
2022-11-08 13:43:09 +0200

    feat: added script path for copying

--- contracts/admin-scripts/scripts/prepare-contracts.sh
+++ contracts/admin-scripts/scripts/prepare-contracts.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 echo $'\n'Cleaning up contracts/ folder
 
-rm -rf contracts
-mkdir contracts
+rm -rf ${SCRIPT_DIR}/../contracts
+mkdir ${SCRIPT_DIR}/../contracts
 
 echo Consolidating smart contracts
 
 echo Copy the bootstrap
-cp -r ../bootstrap/contracts contracts/bootstrap
+cp -r ${SCRIPT_DIR}/../../bootstrap/contracts ${SCRIPT_DIR}/../contracts/bootstrap
 
 echo Copy the did-registry
-cp -r ../did-registry/contracts contracts/did-registry
+cp -r ${SCRIPT_DIR}/../../did-registry/contracts ${SCRIPT_DIR}/../contracts/did-registry
 
 echo Copy the proxy
-cp -r ../proxy/contracts contracts/proxy
+cp -r ${SCRIPT_DIR}/../../proxy/contracts ${SCRIPT_DIR}/../contracts/proxy
 
 echo Copy the timestamp
-cp -r ../timestamp/contracts contracts/timestamp
+cp -r ${SCRIPT_DIR}/../../timestamp/contracts ${SCRIPT_DIR}/../contracts/timestamp
 
 echo Copy the trusted-apps-registry
-cp -r ../trusted-apps-registry/contracts contracts/trusted-apps-registry
+cp -r ${SCRIPT_DIR}/../../trusted-apps-registry/contracts ${SCRIPT_DIR}/../contracts/trusted-apps-registry
 
 echo Copy the trusted-issuers-registry
-cp -r ../trusted-issuers-registry/contracts contracts/trusted-issuers-registry
+cp -r ${SCRIPT_DIR}/../../trusted-issuers-registry/contracts ${SCRIPT_DIR}/../contracts/trusted-issuers-registry
 
 echo Copy the trusted-ledgers-registry
-cp -r ../trusted-ledgers-registry/contracts contracts/trusted-ledgers-registry
+cp -r ${SCRIPT_DIR}/../../trusted-ledgers-registry/contracts ${SCRIPT_DIR}/../contracts/trusted-ledgers-registry
 
 echo Copy the trusted-policies-registry
-cp -r ../trusted-policies-registry/contracts contracts/trusted-policies-registry
+cp -r ${SCRIPT_DIR}/../../trusted-policies-registry/contracts ${SCRIPT_DIR}/../contracts/trusted-policies-registry
 
 echo Copy the trusted-schemas-registry
-cp -r ../trusted-schemas-registry/contracts contracts/trusted-schemas-registry
+cp -r ${SCRIPT_DIR}/../../trusted-schemas-registry/contracts ${SCRIPT_DIR}/../contracts/trusted-schemas-registry
