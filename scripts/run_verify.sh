f3cd47d3ce4c7057aa61244c08b226b5b51185f8
Arnaud Riess <arnaud.riess@ext.ec.europa.eu>
2024-01-25 17:22:46 +0100

    fix puppet deployment to testnet

--- scripts/run_verify.sh
+++ scripts/run_verify.sh
@@ -17,5 +17,5 @@ for entry in $(cat affected.yaml | tr ' ' ':' | cut -d ':' -f 3,5 ) ; do
 done
 for value in "${MCO_ARGUMENTS[@]}" ;do
   echo $value | tr ':' ' '
-  ssh mco /usr/local/bin/verify_container_version_fast.sh test aio $(echo $value | tr ':' ' ')
+  ssh mcov2 /usr/local/bin/verify_container_version_fast.sh test aio $(echo $value | tr ':' ' ')
 done
