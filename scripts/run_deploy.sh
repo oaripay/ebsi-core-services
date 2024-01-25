0a83af514e028e0420f20528e9c051d0b93f173e
Arnaud Riess <arnaud.riess@ext.ec.europa.eu>
2024-01-25 10:44:38 +0100

    jenkins: do fast deployment stage on both legacy and new puppet servers

--- scripts/run_deploy.sh
+++ scripts/run_deploy.sh
@@ -18,5 +18,9 @@ for entry in $(cat affected.yaml | tr ' ' ':' | cut -d ':' -f 3,5 ) ; do
 done
 for value in "${MCO_ARGUMENTS[@]}" ;do
   echo $value | tr ':' ' '
+  echo "force puppet run for hosts managed by legacy puppet server:"
   ssh mco /usr/local/bin/run_faster_containers_only_no_puppet.sh test aio $(echo $value | tr ':' ' ')
+  echo
+  echo "force puppet run for hosts managed by puppet v2 servers:"
+  ssh mcov2 /usr/local/bin/run_faster_containers_only_no_puppet.sh test aio $(echo $value | tr ':' ' ')
 done
