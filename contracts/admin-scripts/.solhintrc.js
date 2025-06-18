9c945eca37a726555d22b9d9bbe9aab6ecd607a9
Yannick Huard <huard.yan@gmail.com>
2025-06-18 16:26:33 +0200

    fix: keep dependencies up-to-date - EBSIINT-11360

--- contracts/admin-scripts/.solhintrc.js
+++ contracts/admin-scripts/.solhintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   rules: {
     // conflicts with prettier
     "bracket-align": "off",
-    "compiler-version": ["error", "^0.8.12"],
+    "compiler-version": ["error", "0.8.12"],
     // we use libraries which trigger this rule too often
     "mark-callable-contracts": "off",
     // inline assembly is needed to store data at a specific location see *Storage.sol
--- contracts/track-and-trace/contracts/TrackAndTrace.sol
+++ contracts/track-and-trace/contracts/TrackAndTrace.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: EUPL V1.2
-pragma solidity ^0.8.12;
+pragma solidity 0.8.12;
 
 import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
