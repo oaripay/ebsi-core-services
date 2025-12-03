28047ecefd3d7c9afab2713b22774adf39cd2cbc
Julian GONZALEZ AGUDELO <julian.gonzalez-agudelo@ext.ec.europa.eu>
2025-12-03 14:16:36 +0000

    fix: handle http exceptions - EBSIINT-12133

--- apis/trusted-schemas-registry-v3/src/filters/http-exception.filter.ts
+++ apis/trusted-schemas-registry-v3/src/filters/http-exception.filter.ts
@@ -15,6 +15,7 @@ import {
   BadRequestException,
   Catch,
   ForbiddenException,
+  HttpException,
   Logger,
   NotFoundException,
   ServiceUnavailableException,
@@ -72,6 +73,14 @@ function getProblemDetailsError(
     // Map to Problem Details error
     return new BadRequestError(BadRequestError.defaultTitle, {
       detail,
+    });
+  }
+
+  if (error instanceof HttpException) {
+    logger.error(error.message, error.stack);
+
+    return new ProblemDetailsError(error.getStatus(), error.message, {
+      detail: error.message,
     });
   }
 
